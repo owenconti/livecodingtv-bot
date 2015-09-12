f3ce153d781b1858656bef6f07bdb259b6a1d768
Owen Conti <owen@ohseemedia.com>
2015-09-12 12:07:01 -0600

    added a voices command

--- commands/greeting.js
+++ commands/greeting.js
@@ -1,6 +1,6 @@
 module.exports = [{
     types: ['message'],
-    regex: /^(hi|hello|hola|hey|greetings|sup)/,
+    regex: /^(hi|hello|hola|hey|greetings|sup)/i,
     action: function( chat, stanza ) {
         chat.replyTo( stanza.fromUsername, 'Welcome!' );
     }
---
+++ commands/voices.js
@@ -0,0 +1,8 @@
+module.exports = [{
+    types: ['message'],
+    regex: /^(!|\/)voices/i,
+    action: function( chat, stanza ) {
+		var voices = `Agnes, Kathy, Princess, Vicki, Victoria, Albert, Alex, Bruce, Fred, Junior, Ralph, Bad News, Bahh, Bells, Boing, Bubbles, Cellos, Deranged, Good News, Hysterical, Pipe Organ, Trinoids, Whisper, Zarvox`;
+        chat.replyTo( stanza.fromUsername, `Available !say voices:\n\n${voices}` );
+    }
+}];
