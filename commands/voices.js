454d1fdeb31b72d2f72056b4ba74c8241108e59b
Owen Conti <owenconti@gmail.com>
2015-10-25 12:06:06 -0600

    fixed fromUsername in voices command

--- commands/voices.js
+++ commands/voices.js
@@ -5,6 +5,6 @@ module.exports = [{
     regex: /^(!|\/)voices/i,
     action: function( chat, stanza ) {
 		var voices = `Agnes, Kathy, Princess, Vicki, Victoria, Albert, Alex, Bruce, Fred, Junior, Ralph, Bad News, Bahh, Bells, Boing, Bubbles, Cellos, Deranged, Good News, Hysterical, Pipe Organ, Trinoids, Whisper, Zarvox`;
-		chat.replyTo( stanza.fromUsername, `Available !say voices:\n\n${voices}` );
+		chat.replyTo( stanza.user.username, `Available !say voices:\n\n${voices}` );
     }
 }];
