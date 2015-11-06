8b85e23be86f7ea63d651544106fcabe72a242e7
Owen Conti <owen@ohseemedia.com>
2015-11-05 23:30:33 -0700

    working on ban management es6 class

--- commands/ban-management.js
+++ commands/ban-management.js
@@ -5,123 +5,146 @@ const Assets = require('../utils/Assets');
 const Websocket = require('../utils/Websocket');
 const Say = require('../utils/Say');
 
-/**
- * Returns a stanza to send to the server.
- * @param  {string} affiliation
- * @return {Stanza}
- */
-var getUserAffiliationStanza = function( credentials, nickname, affiliation ) {
-	var stanza = new xmpp.Stanza('iq', {
-		from: credentials.jid,
-		id: '12:sendIQ',
-		to: credentials.roomJid,
-		type: 'set',
-		xmlns: 'jabber:client'
-	})
-	.c('query', { xmlns: 'http://jabber.org/protocol/muc#admin' })
-	.c('item', { affiliation: affiliation, jid: nickname + '@livecoding.tv' });
-
-	return stanza;
-};
-
-const unbanRegex = new RegExp( /^(!|\/)unban\s(\w+)$/ );
-const banRegex = new RegExp( /^(!|\/)ban\s(\w+)$/ );
-
-module.exports = [{
-	// Auto ban users flood messages to the chat
-	types: ['message'],
-    regex: /./,
-	ignoreRateLimiting: true,
-    action: function( chat, stanza ) {
-		const numberOfMessagesAllowed = 5;
-		const timeframeAllowed = 10; // seconds
-
-		// Never auto ban the streamer or the bot
-		if ( stanza.user.isStreamer() || stanza.user.isBot() ) {
-			return;
-		}
-
-		let userMessageLog = stanza.user.getMessages();
-		if ( userMessageLog ) {
-			let messages = userMessageLog.messages;
-			// If the user has sent at least the number of messages allowed
-			if ( messages.length > numberOfMessagesAllowed ) {
-				const now = new Date().getTime();
-
-				let startOfLimitMessage = messages[ messages.length - numberOfMessagesAllowed ];
-
-				// If the number of allowed messages sent by the user (ie: 5th)
-				// was sent within the last X seconds (timeframeAllowed),
-				// ban the user.
-				if ( now - startOfLimitMessage.time < ( timeframeAllowed * 1000 ) ) {
-					banUser( stanza.user.username, chat );
-				}
-			}
-		}
-
+class BanManagement {
+    static getUnbanRegex() {
+        return new RegExp( /^(!|\/)unban\s(\w+)$/ );
     }
-}, {
-	name: '!unban',
-	help: 'Unbans the specified user.',
-	types: ['message'],
-    regex: unbanRegex,
-    action: function( chat, stanza ) {
-		if ( stanza.user.isModerator() ) {
-			var match = unbanRegex.exec( stanza.message );
-			var userToUnban = match[2];
 
-			var affiliationStanza = getUserAffiliationStanza( chat.credentials, userToUnban, 'none' );
-			chat.client.send( affiliationStanza );
-
-			chat.sendMessage( '@' + userToUnban + ' has been unbanned!' );
-		}
+    static getBanRegex() {
+        return new RegExp( /^(!|\/)ban\s(\w+)$/ );
     }
-}, {
-	name: '!ban',
-	help: 'Bans the specified user.',
-    types: ['message'],
-    regex: banRegex,
-    action: function( chat, stanza ) {
-		if ( stanza.user.isModerator() ) {
-			let match = banRegex.exec( stanza.message );
-			let userToBan = match[2];
 
-			banUser( userToBan, chat );
-		}
+    /**
+     * Returns the commands this class exports.
+     * @return {array}
+     */
+    static getCommands() {
+        return [{
+        	// Auto ban users flood messages to the chat
+        	types: ['message'],
+            regex: /./,
+        	ignoreRateLimiting: true,
+            action: function( chat, stanza ) {
+        		const numberOfMessagesAllowed = 5;
+        		const timeframeAllowed = 10; // seconds
+
+        		// Never auto ban the streamer or the bot
+        		if ( stanza.user.isStreamer() || stanza.user.isBot() ) {
+        			return;
+        		}
+
+        		let userMessageLog = stanza.user.getMessages();
+        		if ( userMessageLog ) {
+        			let messages = userMessageLog.messages;
+        			// If the user has sent at least the number of messages allowed
+        			if ( messages.length > numberOfMessagesAllowed ) {
+        				const now = new Date().getTime();
+
+        				let startOfLimitMessage = messages[ messages.length - numberOfMessagesAllowed ];
+
+        				// If the number of allowed messages sent by the user (ie: 5th)
+        				// was sent within the last X seconds (timeframeAllowed),
+        				// ban the user.
+        				if ( now - startOfLimitMessage.time < ( timeframeAllowed * 1000 ) ) {
+        					banUser( stanza.user.username, chat );
+        				}
+        			}
+        		}
+
+            }
+        }, {
+        	name: '!unban',
+        	help: 'Unbans the specified user.',
+        	types: ['message'],
+            regex: unbanRegex,
+            action: function( chat, stanza ) {
+        		if ( stanza.user.isModerator() ) {
+        			var match = unbanRegex.exec( stanza.message );
+        			var userToUnban = match[2];
+
+        			var affiliationStanza = getUserAffiliationStanza( chat.credentials, userToUnban, 'none' );
+        			chat.client.send( affiliationStanza );
+
+        			chat.sendMessage( '@' + userToUnban + ' has been unbanned!' );
+        		}
+            }
+        }, {
+        	name: '!ban',
+        	help: 'Bans the specified user.',
+            types: ['message'],
+            regex: banRegex,
+            action: function( chat, stanza ) {
+        		if ( stanza.user.isModerator() ) {
+        			let match = banRegex.exec( stanza.message );
+        			let userToBan = match[2];
+
+        			banUser( userToBan, chat );
+        		}
+            }
+        }];
     }
-}];
 
-function banUser( username, chat ) {
-    var affiliationStanza = getUserAffiliationStanza( chat.credentials, username, 'outcast' );
-    chat.client.send( affiliationStanza );
+    /**
+     * Sends a ban stanza
+     * @param  {string} username
+     * @param  {object} chat
+     * @return {void}
+     */
+    static banUser( username, chat ) {
+        let affiliationStanza = getUserAffiliationStanza( chat.credentials, username, 'outcast' );
+        chat.client.send( affiliationStanza );
 
-    Assets.load( 'ban-police.png', function(base64Image) {
-        let htmlContent = `
-<div>
-    <span style="color: #000;
-    font-size: 50px;
-    position: absolute;
-    left: 30px;
-    top: 55px;
-    font-family: Arial;
-    text-transform: uppercase;
-    display: block;
-    text-align: center;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-    width: 440px;
-    padding: 0.5em 0;">${ username }</span>
-    <img src="data:image/png;base64,${base64Image}"  />
-</div>
-`;
-        Websocket.sendMessage( chat.credentials.room, {
-            message: 'flyout',
-            type: 'div',
-            content: htmlContent,
-            duration: 6000,
-            animation: 'flyLeft'
+        // Display the ban police flyout
+        Assets.load( 'ban-police.png', function(base64Image) {
+            let htmlContent = `<div>
+                <span style="color: #000;
+                font-size: 50px;
+                position: absolute;
+                left: 30px;
+                top: 55px;
+                font-family: Arial;
+                text-transform: uppercase;
+                display: block;
+                text-align: center;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                white-space: nowrap;
+                width: 440px;
+                padding: 0.5em 0;">${ username }</span>
+                <img src="data:image/png;base64,${base64Image}"  />
+            </div>`;
+
+            Websocket.sendMessage( chat.credentials.room, {
+                message: 'flyout',
+                type: 'div',
+                content: htmlContent,
+                duration: 6000,
+                animation: 'flyLeft'
+            });
+            Say.say(`${username} You are banned for being a douche`, 'Daniel' );
         });
-        Say.say(`${username} You are banned for being a douche`, 'Daniel' );
-    });
+    }
+
+    /**
+     * Returns a stanza to send to the server.
+     * @param  {object} credentials
+     * @param  {string} nickname
+     * @param  {string} affiliation
+     * @return {Stanza}
+     */
+    static getUserAffiliationStanza( credentials, nickname, affiliation ) {
+    	let stanza = new xmpp.Stanza('iq', {
+    		from: credentials.jid,
+    		id: '12:sendIQ',
+    		to: credentials.roomJid,
+    		type: 'set',
+    		xmlns: 'jabber:client'
+    	})
+    	.c('query', { xmlns: 'http://jabber.org/protocol/muc#admin' })
+    	.c('item', { affiliation: affiliation, jid: nickname + '@livecoding.tv' });
+
+    	return stanza;
+    }
 }
+
+module.exports = BanManagement.getCommands();
